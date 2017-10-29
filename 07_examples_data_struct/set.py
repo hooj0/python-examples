6107d78b654e43d246ae3e9473319ef3ee624c65
hoojo <hoojo@qq.com>
2017-10-29 12:54:19 +0800

    :sparkles: :bento:  Added  python3 `data struct->set` example

--- 07_examples_data_struct/set.py
+++ 07_examples_data_struct/set.py
@@ -31,22 +31,22 @@ API:
 
 # 构造 set，通过set(iterable)方法构造，参数是一个iterable集合
 numbers = set([ 1, 2, 2, 3, 1 ])
-print('numbers set:', numbers)
+print('numbers set:', numbers) # numbers set: {1, 2, 3}
 
 numbers = set((2, 2, 4, 5, 1, 4))
-print('numbers set:', numbers)
+print('numbers set:', numbers) # numbers set: {1, 2, 4, 5}
 
 numbers = set({ 2: '', 3: '22', 5: '32' })
-print('numbers set:', numbers)
+print('numbers set:', numbers) # numbers set: {2, 3, 5}
 
 
 # 添加元素 add，通过add方法添加新元素
 numbers.add(6)
-print('numbers set:', numbers)
+print('numbers set:', numbers) # numbers set: {2, 3, 5, 6}
 
 # 删除元素
 numbers.remove(6)
-print('numbers set:', numbers)
+print('numbers set:', numbers) # numbers set: {2, 3, 5}
 
 # 清空所有元素
 numbers.clear()
@@ -56,12 +56,12 @@ numbers1 = set([ 1, 2, 3, 4 ])
 numbers2 = set([ 2, 4, 6, 8 ])
 
 # 求numbers1、numbers2的交集
-print('交集:', numbers1 & numbers2) 
-print('差集:', numbers1 - numbers2)
-print('差集:', numbers2 - numbers1)  
-print('并集:', numbers1 | numbers2)
-print('排除交集后的元素:', numbers1 ^ numbers2)  
+print('交集:', numbers1 & numbers2) # 交集: {2, 4}
+print('差集:', numbers1 - numbers2) # 差集: {1, 3}
+print('差集:', numbers2 - numbers1) # 差集: {8, 6} 
+print('并集:', numbers1 | numbers2) # 并集: {1, 2, 3, 4, 6, 8}
+print('排除交集后的元素:', numbers1 ^ numbers2)  # 排除交集后的元素: {1, 3, 6, 8}
 
 # 字符串直接被构建成set
 strs = set('Traceback')
-print("set('Traceback')", strs)
+print("set('Traceback')", strs) # set('Traceback') {'k', 'e', 'b', 'a', 'T', 'c', 'r'}
