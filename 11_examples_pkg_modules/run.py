49da94564898156a27388eb02f3ba825ce9acc16
hoojo <hoojo@qq.com>
2018-03-04 16:35:02 +0800

    :sparkles: :bento:  Added  python3 `pkg modules -> run` example

--- 11_examples_pkg_modules/run.py
+++ 11_examples_pkg_modules/run.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-04 16:35:01
 # @copyright by hoojo@2018
-# @changelog Added python3 `pkg modules->run` example
+# @changelog Added python3 `pkg modules -> run` example
 
 
 print('files module')
