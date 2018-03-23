d31dca6504b9b6a6b4919e0ee1ade8ae708eccaa
hoojo <hoojo@qq.com>
2018-03-23 22:17:46 +0800

    :sparkles: :bento:  Added  python3 `os file -> file` example

--- 15_examples_os_file/os_file.py
+++ 15_examples_os_file/os_file.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-23 22:17:46
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os file` example
+# @changelog Added python3 `os file -> file` example
 
 
 import os
