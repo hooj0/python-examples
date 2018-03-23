8cb097a53b73b97e97d6baa192b51d42c49be361
hoojo <hoojo@qq.com>
2018-03-23 22:28:55 +0800

    :sparkles: :bento:  Added  python3 `os dir -> chflags` example

--- 14_examples_os_dir/_os_chflags.py
+++ 14_examples_os_dir/_os_chflags.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-23 22:28:54
 # @copyright by hoojo@2018
-# @changelog Added python3 `os dir-> os chflags` example
+# @changelog Added python3 `os dir -> chflags` example
 
 
 import os
