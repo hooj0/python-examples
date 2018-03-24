daaf0ca9211e2ca81a4643aa685c26e10ca23250
hoojo <hoojo@qq.com>
2018-03-24 16:35:06 +0800

    :sparkles: :bento:  Added  python3 `os dir -> fpathconf` example

--- 14_examples_os_dir/os_fpathconf.py
+++ 14_examples_os_dir/os_fpathconf.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-24 16:35:06
 # @copyright by hoojo@2018
-# @changelog Added python3 `os dir->os fpathconf` example
+# @changelog Added python3 `os dir -> fpathconf` example
 
 
 import os
