bc798321a3c8cf885732873a1dfe61e05aa4c7d4
hoojo <hoojo@qq.com>
2018-03-24 22:54:05 +0800

    :sparkles: :bento:  Added  python3 `os file -> ftruncate` example

--- 15_examples_os_file/os_ftruncate.py
+++ 15_examples_os_file/os_ftruncate.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-24 22:54:04
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os ftruncate` example
+# @changelog Added python3 `os file -> ftruncate` example
 
 
 import os
