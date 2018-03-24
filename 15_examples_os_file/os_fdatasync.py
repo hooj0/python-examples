1cd164cfcc6b732dd5bff96ac35ff0bd786801df
hoojo <hoojo@qq.com>
2018-03-24 13:00:32 +0800

    :sparkles: :bento:  Added  python3 `os file -> fdatasync` example

--- 15_examples_os_file/os_fdatasync.py
+++ 15_examples_os_file/os_fdatasync.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-24 13:00:32
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os fdatasync` example
+# @changelog Added python3 `os file -> fdatasync` example
 
 
 import os
