dd1d1da1c4247cea99b72741975c9b1f69cf135f
hoojo <hoojo@qq.com>
2018-03-24 12:38:54 +0800

    :sparkles: :bento:  Added  python3 `os dir -> fchmod` example

--- 14_examples_os_dir/os_fchmod.py
+++ 14_examples_os_dir/os_fchmod.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-24 12:38:53
 # @copyright by hoojo@2018
-# @changelog Added python3 `os dir->os fchmod` example
+# @changelog Added python3 `os dir -> fchmod` example
 
 
 import os
