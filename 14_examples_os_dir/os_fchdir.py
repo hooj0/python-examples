0f517bfd2ccc9ffda70a71610827224e7e136330
hoojo <hoojo@qq.com>
2018-03-24 10:39:28 +0800

    :sparkles: :bento:  Added  python3 `os dir -> fchdir` example

--- 14_examples_os_dir/os_fchdir.py
+++ 14_examples_os_dir/os_fchdir.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-24 10:39:27
 # @copyright by hoojo@2018
-# @changelog Added python3 `os dir->os fchdir` example
+# @changelog Added python3 `os dir -> fchdir` example
 
 
 import os
