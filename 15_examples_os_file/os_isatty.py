28f81b6183f4f87b0fcf3eb427481305e6555ca3
hoojo <hoojo@qq.com>
2018-03-25 11:48:12 +0800

    :sparkles: :bento:  Added  python3 `os file -> isatty` example

--- 15_examples_os_file/os_isatty.py
+++ 15_examples_os_file/os_isatty.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-25 11:48:11
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os isatty` example
+# @changelog Added python3 `os file -> isatty` example
 
 
 import os
