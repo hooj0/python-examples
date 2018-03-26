6c837fd51848ce287bceb58d2cef9cd3b3ecc18f
hoojo <hoojo@qq.com>
2018-03-26 22:22:28 +0800

    :sparkles: :bento:  Added  python3 `os file -> major` example

--- 15_examples_os_file/os_major.py
+++ 15_examples_os_file/os_major.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-03-26 22:22:27
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os major` example
+# @changelog Added python3 `os file -> major` example
 
 
 import os
