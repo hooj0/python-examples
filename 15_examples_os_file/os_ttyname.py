dac5e38570fd0bf22580d7efd813aa16f6c4128d
hoojo <hoojo@qq.com>
2018-04-01 16:56:11 +0800

    :sparkles: :bento:  Added  python3 `os file -> ttyname` example

--- 15_examples_os_file/os_ttyname.py
+++ 15_examples_os_file/os_ttyname.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 16:56:11
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os ttyname` example
+# @changelog Added python3 `os file -> ttyname` example
 
 
 import os
