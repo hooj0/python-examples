f3678e2936e1272d8da5068b08d23f23bfc5b779
hoojo <hoojo@qq.com>
2018-04-01 16:50:17 +0800

    :sparkles: :bento:  Added  python3 `os file -> tmpnam` example

--- 15_examples_os_file/os_tmpnam.py
+++ 15_examples_os_file/os_tmpnam.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 16:50:17
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os tmpnam` example
+# @changelog Added python3 `os file -> tmpnam` example
 
 
 import os
