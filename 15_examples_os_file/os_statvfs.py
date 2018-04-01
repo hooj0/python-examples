2fcbbd6f007e1973aaf1907a06ca27ce946b7953
hoojo <hoojo@qq.com>
2018-04-01 13:13:22 +0800

    :sparkles: :bento:  Added  python3 `os file -> statvfs` example

--- 15_examples_os_file/os_statvfs.py
+++ 15_examples_os_file/os_statvfs.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 13:13:21
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os statvfs` example
+# @changelog Added python3 `os file -> statvfs` example
 
 
 import os
