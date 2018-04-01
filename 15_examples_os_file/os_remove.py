19f381bf4d951841589f869802bab04eedc19861
hoojo <hoojo@qq.com>
2018-04-01 09:58:15 +0800

    :sparkles: :bento:  Added  python3 `os file -> remove` example

--- 15_examples_os_file/os_remove.py
+++ 15_examples_os_file/os_remove.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 09:58:14
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os remove` example
+# @changelog Added python3 `os file -> remove` example
 
 
 import os
