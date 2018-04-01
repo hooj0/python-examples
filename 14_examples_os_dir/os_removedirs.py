502e036128b301980c43a07e4d16db1b6e139590
hoojo <hoojo@qq.com>
2018-04-01 10:52:17 +0800

    :sparkles: :bento:  Added  python3 `os dir -> removedirs` example

--- 14_examples_os_dir/os_removedirs.py
+++ 14_examples_os_dir/os_removedirs.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 10:52:17
 # @copyright by hoojo@2018
-# @changelog Added python3 `os dir->os removedirs` example
+# @changelog Added python3 `os dir -> removedirs` example
 
 
 import os
