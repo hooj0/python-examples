6f1be889e6d793847c2ea2dc9509b760214900fc
hoojo <hoojo@qq.com>
2018-04-01 12:27:01 +0800

    :sparkles: :bento:  Added  python3 `os file -> renames` example

--- 15_examples_os_file/os_renames.py
+++ 15_examples_os_file/os_renames.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 12:27:00
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os renames` example
+# @changelog Added python3 `os file -> renames` example
 
 
 import os
