2f6604771f50b54237e6a65c986f912b1aea1847
hoojo <hoojo@qq.com>
2018-04-01 12:50:29 +0800

    :sparkles: :bento:  Added  python3 `os file -> stat` example

--- 15_examples_os_file/os_stat.py
+++ 15_examples_os_file/os_stat.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 12:50:28
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os stat` example
+# @changelog Added python3 `os file -> stat` example
 
 
 import os
