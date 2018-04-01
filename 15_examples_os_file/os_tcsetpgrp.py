646e77df6f9ce6ea2ce78a5dc9ee237ba4e14a04
hoojo <hoojo@qq.com>
2018-04-01 16:19:26 +0800

    :sparkles: :bento:  Added  python3 `os file -> tcsetpgrp` example

--- 15_examples_os_file/os_tcsetpgrp.py
+++ 15_examples_os_file/os_tcsetpgrp.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 16:19:25
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os tcsetpgrp` example
+# @changelog Added python3 `os file -> tcsetpgrp` example
 
 
 import os
