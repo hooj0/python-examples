23949a74eef4c27d4e5ebd83fd2a58eed66464ca
hoojo <hoojo@qq.com>
2018-04-01 18:09:37 +0800

    :sparkles: :bento:  Added  python3 `object -> init` example

--- 17_examples_object/init.py
+++ 17_examples_object/init.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 18:09:37
 # @copyright by hoojo@2018
-# @changelog Added python3 `object->init` example
+# @changelog Added python3 `object -> init` example
 
 
 class Student:
