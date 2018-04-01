827b3d1ba1726c0b562ffbb40780c46a1c3a7dbc
hoojo <hoojo@qq.com>
2018-04-01 11:57:00 +0800

    :sparkles: :bento:  Added  python3 `os file -> rename` example

--- 15_examples_os_file/os_rename.py
+++ 15_examples_os_file/os_rename.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 11:56:59
 # @copyright by hoojo@2018
-# @changelog Added python3 `os file->os rename` example
+# @changelog Added python3 `os file -> rename` example
 
 
 import os
