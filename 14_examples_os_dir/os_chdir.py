c457b0c52dff4e6f2a7f426e8d30f396584762f9
hoojo <hoojo@qq.com>
2018-04-01 12:28:28 +0800

    :sparkles: :bento:  Added  python3 `os dir -> chdir` example

--- 14_examples_os_dir/os_chdir.py
+++ 14_examples_os_dir/os_chdir.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-01 12:28:28
 # @copyright by hoojo@2018
-# @changelog Added python3 `os dir->os chdir` example
+# @changelog Added python3 `os dir -> chdir` example
 
 
 import os
