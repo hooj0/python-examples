dc337dbaa3abe9982dc966859db2ac97e9ddf998
hoojo <hoojo@qq.com>
2018-04-03 22:04:47 +0800

    :sparkles: :bento:  Added  python3 `standand lib -> os lib` example

--- 18_examples_standand_lib/os_lib.py
+++ 18_examples_standand_lib/os_lib.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-03 22:04:47
 # @copyright by hoojo@2018
-# @changelog Added python3 `standand lib->os lib` example
+# @changelog Added python3 `standand lib -> os lib` example
 
 
 import os
