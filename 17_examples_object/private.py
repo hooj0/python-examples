a7e33a0cbe28884605d62dd6afed0522093e972b
hoojo <hoojo@qq.com>
2018-04-03 21:05:09 +0800

    :sparkles: :bento:  Added  python3 `object -> private` example

--- 17_examples_object/private.py
+++ 17_examples_object/private.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-03 21:05:08
 # @copyright by hoojo@2018
-# @changelog Added python3 `object->private` example
+# @changelog Added python3 `object -> private` example
 
 
 class Private:
