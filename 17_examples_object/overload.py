1abbfaf1f0e164270cceae20d49e58f9f7e8fb2f
hoojo <hoojo@qq.com>
2018-04-03 21:48:26 +0800

    :sparkles: :bento:  Added  python3 `object -> overload` example

--- 17_examples_object/overload.py
+++ 17_examples_object/overload.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-03 21:48:25
 # @copyright by hoojo@2018
-# @changelog Added python3 `object->overload` example
+# @changelog Added python3 `object -> overload` example
 
 
 '''
