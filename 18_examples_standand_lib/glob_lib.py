fe85e34092b7db737dd8cc1022f9a7fa88eaff90
hoojo <hoojo@qq.com>
2018-04-03 22:30:35 +0800

    :sparkles: :bento:  Added  python3 `standand lib -> glob lib` example

--- 18_examples_standand_lib/glob_lib.py
+++ 18_examples_standand_lib/glob_lib.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-03 22:30:35
 # @copyright by hoojo@2018
-# @changelog Added python3 `standand lib->glob lib` example
+# @changelog Added python3 `standand lib -> glob lib` example
 
 
 import glob
