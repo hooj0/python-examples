3fc3cb7f3c9dbc9af7a25f92df8c5c4cbb57b1c5
hoojo <hoojo@qq.com>
2018-04-05 17:07:35 +0800

    :sparkles: :bento:  Added  python3 `standand lib -> random lib` example

--- 18_examples_standand_lib/random_lib.py
+++ 18_examples_standand_lib/random_lib.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-05 17:07:34
 # @copyright by hoojo@2018
-# @changelog Added python3 `standand lib->random lib` example
+# @changelog Added python3 `standand lib -> random lib` example
 
 
 import random
