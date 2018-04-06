a43001419d354f8d0b584a0393446c985e74744b
hoojo <hoojo@qq.com>
2018-04-06 11:01:59 +0800

    :sparkles: :bento:  Added  python3 `standand lib -> doctest lib` example

--- 18_examples_standand_lib/doctest_lib.py
+++ 18_examples_standand_lib/doctest_lib.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-06 11:01:59
 # @copyright by hoojo@2018
-# @changelog Added python3 `standand lib->doctest lib` example
+# @changelog Added python3 `standand lib -> doctest lib` example
 
 
 import doctest
