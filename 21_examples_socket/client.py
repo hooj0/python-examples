9a3ca56bb1140d7ae43d2541f1db85eca6fa2702
hoojo <hoojo@qq.com>
2018-04-10 22:57:35 +0800

    :sparkles: :bento:  Added  python3 `socket -> client` example

--- 21_examples_socket/client.py
+++ 21_examples_socket/client.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-10 22:57:34
 # @copyright by hoojo@2018
-# @changelog Added python3 `socket->client` example
+# @changelog Added python3 `socket -> client` example
 
 
 # Echo client program
