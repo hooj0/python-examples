091832515612ebc7e77df16e041d6897a0e2c045
hoojo <hoojo@qq.com>
2018-04-12 22:42:59 +0800

    :sparkles: :bento:  Added  python3 `socket -> sk` example

--- 21_examples_socket/sk.py
+++ 21_examples_socket/sk.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-12 22:42:58
 # @copyright by hoojo@2018
-# @changelog Added python3 `socket->sk` example
+# @changelog Added python3 `socket -> sk` example
 
 
 import socket
