72c8f56668ac10e2ce17257eb3e666f7dab18fc6
hoojo <hoojo@qq.com>
2018-04-14 14:55:33 +0800

    :sparkles: :bento:  Added  python3 `email -> send mail` example

--- 22_examples_email/send_mail.py
+++ 22_examples_email/send_mail.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-14 14:55:33
 # @copyright by hoojo@2018
-# @changelog Added python3 `email->send mail` example
+# @changelog Added python3 `email -> send mail` example
 
 
 from email.mime.text import MIMEText
