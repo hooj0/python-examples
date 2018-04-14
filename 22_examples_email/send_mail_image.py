65040a1c6173222f2f33a16c2fd882fc72c0bb62
hoojo <hoojo@qq.com>
2018-04-14 18:37:55 +0800

    :sparkles: :bento:  Added  python3 `email -> send mail image` example

--- 22_examples_email/send_mail_image.py
+++ 22_examples_email/send_mail_image.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-14 18:37:55
 # @copyright by hoojo@2018
-# @changelog Added python3 `email->send mail image` example
+# @changelog Added python3 `email -> send mail image` example
 
 
 import smtplib
