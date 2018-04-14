5109c45f36ac389ef1ba9f6c8f52eaa796574310
hoojo <hoojo@qq.com>
2018-04-14 23:01:28 +0800

    :sparkles: :bento:  Added  python3 `db -> db insert` example

--- 24_examples_db/db_insert.py
+++ 24_examples_db/db_insert.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-14 23:01:28
 # @copyright by hoojo@2018
-# @changelog Added python3 `db->db insert` example
+# @changelog Added python3 `db -> db insert` example
 
 
 import pymysql
