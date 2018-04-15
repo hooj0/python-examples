79294c83b9807b2119a326ae320ad6bc79489808
hoojo <hoojo@qq.com>
2018-04-15 14:27:15 +0800

    :sparkles: :bento:  Added  python3 `db -> query one` example

--- 24_examples_db/db_query_one.py
+++ 24_examples_db/db_query_one.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-15 14:27:15
 # @copyright by hoojo@2018
-# @changelog Added python3 `db->db query one` example
+# @changelog Added python3 `db -> query one` example
 
 
 '''
