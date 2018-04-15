c2515fcfceb44f29712dbdab01ebefb683ac4a5c
hoojo <hoojo@qq.com>
2018-04-15 14:42:30 +0800

    :sparkles: :bento:  Added  python3 `db -> db delete` example

--- 24_examples_db/db_delete.py
+++ 24_examples_db/db_delete.py
@@ -5,7 +5,7 @@
 # @github: https://github.com/hooj0
 # @create date: 2018-04-15 14:42:29
 # @copyright by hoojo@2018
-# @changelog Added python3 `db->db delete` example
+# @changelog Added python3 `db -> db delete` example
 
 
 import pymysql
