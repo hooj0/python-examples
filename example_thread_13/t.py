65860213dc75a0f195f5c9156c62ce78ca410951
hoojo <hoojo@qq.com>
2018-04-19 21:46:44 +0800

    :art: thread examples

---
+++ example_thread_13/t.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+import threading
+from threading import Timer
+
+data = threading.local()
+data.x = 109
+
+def hello():
+    print("hello, world")
+
+
+# 定时3秒后运行
+t = Timer(3.0, hello)
+t.start()  # after 30 seconds, "hello, world" will be printed
