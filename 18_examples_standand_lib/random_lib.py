d2428665e13a52739094c6e82be4183b5d2baa6e
hoojo <hoojo@qq.com>
2018-06-13 21:57:48 +0800

    :art: Standand Lib  examples

---
+++ 18_examples_standand_lib/random_lib.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @author: hoojo
+import random
+
+print(help(random))
+
+# 随机取数组中一个值
+print(random.choice(['apple', 'pear', 'banana'])) #banana
+
+# 随机生成一个长度为10的数组
+print(random.sample(range(100), 10)) # [84, 6, 62, 12, 79, 82, 86, 55, 27, 41]
+
+# 产生随机float数
+print(random.random()) #0.7395803807672576
+
+# 随机生成一个小于6的随机数
+print(random.randrange(6)) # 3
+
+# 随机生成一个数组
+print(random.choices(['apple', 'pear', 'banana'])) #['banana']
+
+# 随机生成1~10的整数
+print(random.randint(1, 10))
+
+print(random.gauss(3, 10))
