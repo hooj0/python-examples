3c0ea74804dd57252a773d3d5e8d8ff4d2604944
hoojo <hoojo@qq.com>
2018-06-13 21:56:08 +0800

    :art: Exception examples

---
+++ 16_examples_exception/clean_exception.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Created on 2018-03-20
+# @author: hoojo
+
+
+try:
+    raise KeyboardInterrupt
+except:
+    print('exception!')
+finally:
+    print('clean！')    
+    
+    
+    
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError as e:
+        print('zero error:', e)
+    else:
+        print('result:', result)
+    finally:
+        print('divide execute finish!')             
+    
+    
+divide(10, 1)
+'''
+result: 10.0
+divide execute finish!
+'''
+
+divide(10, 0)
+'''
+zero error: division by zero
+divide execute finish!
+'''
+
+divide('10', '2')
+'''
+error
+'''  
+
+
+for line in open("myfile.txt"):
+    print(line, end="")
+# 以上这段代码的问题是，当执行完毕后，文件会保持打开状态，并没有被关闭。
+
+
+# 关键词 with 语句就可以保证诸如文件之类的对象在使用完之后一定会正确的执行他的清理方法:
+with open("myfile.txt") as f:
+    for line in f:
+        print(line, end="")
+# 以上这段代码执行完毕后，就算在处理过程中出问题了，文件 f 总是会关闭      
---
+++ 16_examples_exception/exception.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Created on 2018-03-18
+# @author: hoojo
+
+try:
+    x = int(input('请输入一个数字：'))
+except: # 捕获任意异常
+    print('你输入的是一个非数字字符')
+        
+
+try:
+    int('a')
+except ValueError: # 捕获指导异常
+    print('类型错误')
+
+
+try:
+    int('a')
+except ValueError as e: # 转换异常数据
+    print('类型错误: ', e)
+    
+try:
+    #int('a')
+    int(a)
+except (RuntimeError, TypeError, NameError, ValueError) as e: # 捕获多个异常
+    print('发生多个异常: ', e)  
+    
+    
+import sys
+
+# 多次捕获异常
+try:
+    f = open('myfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print("OS error: {0}".format(err))
+except ValueError:
+    print("Could not convert data to an integer.")
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise # 未知异常，再次抛出         
+
+
+# try except 语句还有一个可选的else子句，如果使用这个子句，那么必须放在所有的except子句之后。
+# 这个子句将在try子句没有发生任何异常的时候执行
+try:
+    arg = '/a.py'
+    f = open(arg, 'r') # 发生异常，else不被执行
+except IOError:
+    print('cannot open', arg)
+else:
+    print(arg, 'has', len(f.readlines()), 'lines')
+    f.close()
+    
+try:
+    f = int('1') # 没有异常，else被执行
+except:
+    print('cannot open', f)
+else:
+    print('input number ', f)
+    
+    
