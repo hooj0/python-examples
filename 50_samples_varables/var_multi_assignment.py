c1ddd1aa77dc75f0d8af8247dfcc5c6da3bc7924
hoojo <hoojo@qq.com>
2018-07-01 15:11:33 +0800

    :bento: :art: :spartkles: DataStruct varables assignment value examples

--- 50_samples_varables/var_multi_assignment.py
+++ 50_samples_varables/var_multi_assignment.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email:    hoojo_@126.com
+# @github:   https://github.com/hooj0
+# @create date: 2018-06-28
+# @copyright by hoojo @ 2018
 
 
 #===============================================================================
