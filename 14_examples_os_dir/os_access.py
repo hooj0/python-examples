2afaadd5bb5e0b2e7cbe0740cb89d7b4fb6a94e4
hoojo <hoojo@qq.com>
2018-08-26 19:32:06 +0800

    :sparkles: :bento:  Added  python3 `os dir->os access` example

--- 14_examples_os_dir/os_access.py
+++ 14_examples_os_dir/os_access.py
@@ -1,6 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-03-23 22:15:37
+# @copyright by hoojo@2018
+# @changelog Added python3 `os dir->os access` example
+
+
 import os
 
 
