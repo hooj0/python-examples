3436650f6bd31033ea33fe97427ac306fe0797a1
hoojo <hoojo@qq.com>
2018-08-26 19:32:20 +0800

    :sparkles: :bento:  Added  python3 `os dir->os lchown` example

--- 14_examples_os_dir/os_lchown.py
+++ 14_examples_os_dir/os_lchown.py
@@ -1,6 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-03-25 22:44:01
+# @copyright by hoojo@2018
+# @changelog Added python3 `os dir->os lchown` example
+
+
 import os
 
 '''
