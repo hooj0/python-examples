4d1b9b77a77d385b3e17c2a29fbb9aeef041a692
hoojo <hoojo@qq.com>
2018-08-26 19:31:48 +0800

    :sparkles: :bento:  Added  python3 `pkg modules->packages->core->filters->  init  ` example

--- 11_examples_pkg_modules/packages/core/filters/__init__.py
+++ 11_examples_pkg_modules/packages/core/filters/__init__.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-11-18 17:18:21
+# @copyright by hoojo@2018
+# @changelog Added python3 `pkg modules->packages->core->filters->  init  ` example
+
 
 print('import core.filters')
