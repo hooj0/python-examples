bb95c93eaacfbd6cadd5dc7f991fc4e45a0e7ac0
hoojo <hoojo@qq.com>
2018-08-26 19:31:52 +0800

    :sparkles: :bento:  Added  python3 `pkg modules->packages->core->parser->parser` example

--- 11_examples_pkg_modules/packages/core/parser/parser.py
+++ 11_examples_pkg_modules/packages/core/parser/parser.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-11-18 17:19:05
+# @copyright by hoojo@2018
+# @changelog Added python3 `pkg modules->packages->core->parser->parser` example
+
 
 parser_name = 'my parser'
 
