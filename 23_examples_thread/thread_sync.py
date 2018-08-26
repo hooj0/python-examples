43d44b364fab20addd88ed0cec79a7c2b0ff9042
hoojo <hoojo@qq.com>
2018-08-26 19:33:51 +0800

    :sparkles: :bento:  Added  python3 `thread->thread sync` example

--- 23_examples_thread/thread_sync.py
+++ 23_examples_thread/thread_sync.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-16 23:05:15
+# @copyright by hoojo@2018
+# @changelog Added python3 `thread->thread sync` example
+
+
 import threading
 import time
 
