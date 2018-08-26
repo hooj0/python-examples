c5b2030f7fbcb748ccfd1b594176c944b9536e49
hoojo <hoojo@qq.com>
2018-08-26 19:31:38 +0800

    :sparkles: :bento:  Added  python3 `pkg modules->modules->module center` example

--- 11_examples_pkg_modules/modules/module_center.py
+++ 11_examples_pkg_modules/modules/module_center.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-11-14 22:47:58
+# @copyright by hoojo@2018
+# @changelog Added python3 `pkg modules->modules->module center` example
+
+
 import keyword
 
 module_name = 'module_center'
