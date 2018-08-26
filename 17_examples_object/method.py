71e8d20af3f5df1ba0acb8be60db8d3158f79d4f
hoojo <hoojo@qq.com>
2018-08-26 19:33:16 +0800

    :sparkles: :bento:  Added  python3 `object->method` example

--- 17_examples_object/method.py
+++ 17_examples_object/method.py
@@ -1,6 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-01 18:36:32
+# @copyright by hoojo@2018
+# @changelog Added python3 `object->method` example
 
 
 class Person:
