01b63797fa201bab454fe9779e7a80067281b633
hoojo <hoojo@qq.com>
2018-08-26 19:33:59 +0800

    :sparkles: :bento:  Added  python3 `db->db insert format` example

--- 24_examples_db/db_insert_format.py
+++ 24_examples_db/db_insert_format.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-15 14:19:29
+# @copyright by hoojo@2018
+# @changelog Added python3 `db->db insert format` example
+
+
 import pymysql
   
 # 打开数据库连接  
