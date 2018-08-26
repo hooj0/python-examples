c69ae1511d9943664762a223d9d5f125f61e993b
hoojo <hoojo@qq.com>
2018-08-26 19:33:50 +0800

    :sparkles: :bento:  Added  python3 `thread->thread schedul` example

--- 23_examples_thread/thread_schedul.py
+++ 23_examples_thread/thread_schedul.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-18 22:49:55
+# @copyright by hoojo@2018
+# @changelog Added python3 `thread->thread schedul` example
 
 
 import threading
