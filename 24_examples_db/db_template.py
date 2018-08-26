d0aecfcfad041cbe62a357c07eb1d369057de69f
hoojo <hoojo@qq.com>
2018-08-26 19:34:06 +0800

    :sparkles: :bento:  Added  python3 `db->db template` example

--- 24_examples_db/db_template.py
+++ 24_examples_db/db_template.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-15 16:07:27
+# @copyright by hoojo@2018
+# @changelog Added python3 `db->db template` example
+
 
 import pymysql
 
