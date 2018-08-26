404c3878b94c317190bacc970f85c7d12cda7695
hoojo <hoojo@qq.com>
2018-08-26 19:34:09 +0800

    :sparkles: :bento:  Added  python3 `shell->shell cmd` example

--- 25_examples_shell/shell_cmd.py
+++ 25_examples_shell/shell_cmd.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-21 09:52:29
+# @copyright by hoojo@2018
+# @changelog Added python3 `shell->shell cmd` example
+
 
 import subprocess
 
