b43d253842ff580087bcaa3a5a2b20b037c4ae39
hoojo <hoojo@qq.com>
2018-08-26 19:32:10 +0800

    :sparkles: :bento:  Added  python3 `os dir->os chroot` example

--- 14_examples_os_dir/os_chroot.py
+++ 14_examples_os_dir/os_chroot.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-03-24 09:43:50
+# @copyright by hoojo@2018
+# @changelog Added python3 `os dir->os chroot` example
+
 
 import os
 
