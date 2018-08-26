703735b1a7116b61e23836f7061c29dbef20bfe4
hoojo <hoojo@qq.com>
2018-08-26 19:31:24 +0800

    :sparkles: :bento:  Added  python3 `math->math operator` example

--- 05_examples_math/math_operator.py
+++ 05_examples_math/math_operator.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-10-23 22:25:32
+# @copyright by hoojo@2018
+# @changelog Added python3 `math->math operator` example
+
 
 # math_operator.py
 
