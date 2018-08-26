d61e9ed456e2e52fa552e1c1d9a31b263cb1dadf
hoojo <hoojo@qq.com>
2018-08-26 19:31:10 +0800

    :sparkles: :bento:  Added  python3 `helloworld->hello world` example

--- 00_examples_helloworld/hello_world.py
+++ 00_examples_helloworld/hello_world.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-10-21 17:12:36
+# @copyright by hoojo@2018
+# @changelog Added python3 `helloworld->hello world` example
+
 
 # hello_world.py
 
