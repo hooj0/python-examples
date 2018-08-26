11f72764460399af55354d2a743f43f3981b3e45
hoojo <hoojo@qq.com>
2018-08-26 19:33:09 +0800

    :sparkles: :bento:  Added  python3 `exception->custom exception` example

--- 16_examples_exception/custom_exception.py
+++ 16_examples_exception/custom_exception.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
-# Created on 2018-03-20
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-03-20 21:38:27
+# @copyright by hoojo@2018
+# @changelog Added python3 `exception->custom exception` example
+
 
 class MyError(Exception):
     def __init__(self, value):
