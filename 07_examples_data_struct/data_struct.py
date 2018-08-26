78fe2ca1c8b3a7f4d418797fb63e9c2005197b93
hoojo <hoojo@qq.com>
2018-08-26 19:31:27 +0800

    :sparkles: :bento:  Added  python3 `data struct->data struct` example

--- 07_examples_data_struct/data_struct.py
+++ 07_examples_data_struct/data_struct.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
-# Created on 2017-11-11
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-11-11 23:05:07
+# @copyright by hoojo@2018
+# @changelog Added python3 `data struct->data struct` example
+
+
 from _collections import deque
 
 '''
