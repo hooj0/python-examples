8c01e96188ad0530e112411d7c87fe7806d788f9
hoojo <hoojo@qq.com>
2018-08-26 19:34:08 +0800

    :sparkles: :bento:  Added  python3 `db->db update` example

--- 24_examples_db/db_update.py
+++ 24_examples_db/db_update.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-15 14:35:52
+# @copyright by hoojo@2018
+# @changelog Added python3 `db->db update` example
+
+
 import pymysql
   
 # 打开数据库连接  
