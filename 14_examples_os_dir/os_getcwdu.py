b3394e61f8a110fba42527be34f34a1c63839853
hoojo <hoojo@qq.com>
2018-08-26 19:32:19 +0800

    :sparkles: :bento:  Added  python3 `os dir->os getcwdu` example

--- 14_examples_os_dir/os_getcwdu.py
+++ 14_examples_os_dir/os_getcwdu.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-03-25 11:26:42
+# @copyright by hoojo@2018
+# @changelog Added python3 `os dir->os getcwdu` example
+
 
 import os
 
