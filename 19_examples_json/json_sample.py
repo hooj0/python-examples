2663fe87af1f1d0bc0341c2469d32818cd43aa0b
hoojo <hoojo@qq.com>
2018-08-26 19:33:31 +0800

    :sparkles: :bento:  Added  python3 `json->json sample` example

--- 19_examples_json/json_sample.py
+++ 19_examples_json/json_sample.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-18 22:59:56
+# @copyright by hoojo@2018
+# @changelog Added python3 `json->json sample` example
+
+
 import json
 
 
