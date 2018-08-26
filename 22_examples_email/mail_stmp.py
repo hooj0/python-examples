bc2de148fc6fa72926647dbdcac1657f3f48637d
hoojo <hoojo@qq.com>
2018-08-26 19:33:41 +0800

    :sparkles: :bento:  Added  python3 `email->mail stmp` example

--- 22_examples_email/mail_stmp.py
+++ 22_examples_email/mail_stmp.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-09 23:09:36
+# @copyright by hoojo@2018
+# @changelog Added python3 `email->mail stmp` example
+
 
 '''
 Python创建 SMTP 对象语法如下：
