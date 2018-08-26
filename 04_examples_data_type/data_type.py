0b7b3dcca2a454810372bd0d9fd84334bd67ef0e
hoojo <hoojo@qq.com>
2018-08-26 19:31:21 +0800

    :sparkles: :bento:  Added  python3 `data type->data type` example

--- 04_examples_data_type/data_type.py
+++ 04_examples_data_type/data_type.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-10-23 20:15:44
+# @copyright by hoojo@2018
+# @changelog Added python3 `data type->data type` example
+
 
 # data_type.py
 
