1c387c150a52f3176a7381c18e2b3609be04db01
hoojo <hoojo@qq.com>
2018-08-26 19:33:49 +0800

    :sparkles: :bento:  Added  python3 `thread->thread queue` example

--- 23_examples_thread/thread_queue.py
+++ 23_examples_thread/thread_queue.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2018-04-16 23:10:21
+# @copyright by hoojo@2018
+# @changelog Added python3 `thread->thread queue` example
+
+
 import queue
 import threading
 import time
