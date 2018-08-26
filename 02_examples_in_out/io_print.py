4ff4164bb99b87e1742ece93aeab39eb7f3ce04b
hoojo <hoojo@qq.com>
2018-08-26 19:31:14 +0800

    :sparkles: :bento:  Added  python3 `in out->io print` example

--- 02_examples_in_out/io_print.py
+++ 02_examples_in_out/io_print.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# encoding: utf-8
+# @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-10-21 18:38:51
+# @copyright by hoojo@2018
+# @changelog Added python3 `in out->io print` example
+
 
 # print.py
 
