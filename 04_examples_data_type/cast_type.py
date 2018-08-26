57db10c34781ad32c0a5a570dae0fb2400d21fd0
hoojo <hoojo@qq.com>
2018-08-26 19:31:20 +0800

    :sparkles: :bento:  Added  python3 `data type->cast type` example

--- 04_examples_data_type/cast_type.py
+++ 04_examples_data_type/cast_type.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
-# Created on 2017-11-04
 # @author: hoojo
+# @email: hoojo_@126.com
+# @github: https://github.com/hooj0
+# @create date: 2017-11-04 20:46:17
+# @copyright by hoojo@2018
+# @changelog Added python3 `data type->cast type` example
+
 
 '''
 类型转换
