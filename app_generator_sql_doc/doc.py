8b8b843b8767d08cd4582aebd62dc6ef25b748e9
hoojo <hoojo@qq.com>
2020-03-05 19:47:05 +0800

    :sparkles: :bento: :recycle:  Changed doc py

--- app_generator_sql_doc/doc.py
+++ app_generator_sql_doc/doc.py
@@ -29,10 +29,12 @@ records = (
 )
 
 table = document.add_table(rows=1, cols=3)
+
 hdr_cells = table.rows[0].cells
 hdr_cells[0].text = 'Qty'
 hdr_cells[1].text = 'Id'
 hdr_cells[2].text = 'Desc'
+
 for qty, id, desc in records:
     row_cells = table.add_row().cells
     row_cells[0].text = str(qty)
