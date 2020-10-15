87f2a4521b28a3f705a9a123562eb8c6e9e3c36d
hoojo <hoojo@qq.com>
2020-10-15 09:41:17 +0800

    :sparkles: :bento: :recycle:  Changed python3 `asyncio advanced -> queue` example

--- 29_examples_asyncio_advanced/asyncio_advanced_stretch_queue.py
+++ 29_examples_asyncio_advanced/asyncio_advanced_stretch_queue.py
@@ -5,11 +5,11 @@
 # @github: https://github.com/hooj0
 # @create date: 2020-10-14
 # @copyright by hoojo@2018
-# @changelog python3 `asyncio advanced -> condition` example
+# @changelog python3 `asyncio advanced -> queue` example
 
 
 # ===============================================================================
-# 标题：python3 asyncio advanced condition example
+# 标题：python3 asyncio advanced queue example
 # ===============================================================================
 # 使用：asyncio模块作用，构建协程并发应用的工具
 #
@@ -19,55 +19,65 @@
 # 2、threading模块：多线程并发处理
 # 3、asyncio模块：协程并发处理
 # -------------------------------------------------------------------------------
-# 描述：asyncio.Condition() 对事件状态单独通知执行
+# 描述：协程队列Queue,生产者与消费者的示例
 # -------------------------------------------------------------------------------
 import asyncio
 import functools
 
 
 # -------------------------------------------------------------------------------
-# 对事件状态单独通知执行
+# 协程队列Queue
 # -------------------------------------------------------------------------------
-async def consumer(condition, i):
-    print("[consumer] start")
+async def consumer(i, queue):
+    print("[consumer-%s] start " % i)
 
-    async with condition:
-        print("[consumer] waiting ", i)
-        await condition.wait()
-        print("[consumer] trigger ", i)
+    while True:
+        print("[consumer-%s] waiting " % i)
 
-    print("[consumer] finished")
+        item = await queue.get()
+        print("[consumer-%s] get queue: %s" % (i, item))
 
+        queue.task_done()
+        if item is None:
+            # queue.task_done()
+            break
+        else:
+            await asyncio.sleep(0.01 * item)
+            # queue.task_done()
+            print("[consumer-%s] sleep" % i)
 
-async def publisher_notify(condition):
-    print("[publisher_notify] start")
-
-    await asyncio.sleep(0.1)
-    for i in range(3):
-        async with condition:
-            print("[publisher_notify] notify consumer ", i)
-            condition.notify(i)
-
-    async with condition:
-        print("[publisher_notify] notify other consumer")
-        condition.notify_all()
-
-    print("[publisher_notify] finished")
+    print("[consumer-%s] finished " % i)
 
 
-async def call_work():
+async def producer(size, queue):
+    print("[producer] start")
+
+    for i in range(3 * size):
+        await queue.put(i)
+        print("[producer] queue put: ", i)
+
+    print("[producer] queue put None stopped")
+    for _ in range(size):
+        await queue.put(None)
+
+    print("[producer] queue join clear")
+    await queue.join()
+    print("[producer] finished")
+
+
+async def call_work(size):
     print("[call_work] run...")
 
-    # 创建一个操作状态的对象
-    condition = asyncio.Condition()
-    # 5个消费者函数
-    consumers = [consumer(condition, i) for i in range(5)]
-    # 创建一个发布通知的任务
-    loop.create_task(publisher_notify(condition))
+    # 创建一个队列，最大的长度是 size
+    queue = asyncio.Queue(maxsize=size)
+    # 创建消费者
+    consumers = [loop.create_task(consumer(i, queue)) for i in range(size)]
+    # 创建一个生产者的任务
+    producers = loop.create_task(producer(size, queue))
 
-    print("[call_work] wait consumers run")
-    # 等待consumers所有的函数执行完成
-    await asyncio.wait(consumers)
+    print("[call_work] wait consumers/producers run")
+    # 等待consumers, producers 所有的函数执行完成
+    await asyncio.wait(consumers + [producers])
 
     print("[call_work] run finished")
 
@@ -76,7 +86,7 @@ loop = asyncio.get_event_loop()
 
 try:
     print("going event loop")
-    loop.run_until_complete(call_work())
+    loop.run_until_complete(call_work(2))
 finally:
     print("close event loop")
     loop.close()
@@ -86,40 +96,36 @@ finally:
 # ---------------------------------------------------------------------------
 # going event loop
 # [call_work] run...
-# [call_work] wait consumers run
-#
-# 运行发布者，发送通知
-# [publisher_notify] start
-#
-# 消费者开始运行，等待接收通知
-# [consumer] start
-# [consumer] waiting  2
-# [consumer] start
-# [consumer] waiting  3
-# [consumer] start
-# [consumer] waiting  4
-# [consumer] start
-# [consumer] waiting  1
-# [consumer] start
+# [call_work] wait consumers/producers run
+# [consumer] start  0
 # [consumer] waiting  0
-#
-# 发布者 通知消各个费者
-# [publisher_notify] notify consumer  0
-# [publisher_notify] notify consumer  1
-# [publisher_notify] notify consumer  2
-# [publisher_notify] notify other consumer
-# [publisher_notify] finished
-#
-# 接收到通知，消费者运行 等待的后续业务
-# [consumer] trigger  2
-# [consumer] finished
-# [consumer] trigger  3
-# [consumer] finished
-# [consumer] trigger  4
-# [consumer] finished
-# [consumer] trigger  1
-# [consumer] finished
-# [consumer] trigger  0
-# [consumer] finished
+# [consumer] start  1
+# [consumer] waiting  1
+# [producer] start
+# [producer] queue put:  0
+# [producer] queue put:  1
+# [consumer] 0 get queue: 0
+# [consumer] 1 get queue: 1
+# [producer] queue put:  2
+# [producer] queue put:  3
+# [consumer] waiting  0
+# [consumer] 0 get queue: 2
+# [producer] queue put:  4
+# [consumer] waiting  1
+# [consumer] 1 get queue: 3
+# [producer] queue put:  5
+# [producer] queue put None stopped
+# [consumer] waiting  0
+# [consumer] 0 get queue: 4
+# [consumer] waiting  1
+# [consumer] 1 get queue: 5
+# [producer] queue join clear
+# [consumer] waiting  0
+# [consumer] 0 get queue: None
+# [consumer] finished  0
+# [consumer] waiting  1
+# [consumer] 1 get queue: None
+# [consumer] finished  1
+# [producer] finished
 # [call_work] run finished
 # close event loop
