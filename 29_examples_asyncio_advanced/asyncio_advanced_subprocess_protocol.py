a86ba845d316c84b1578b0980819049515445b1e
hoojo <hoojo@qq.com>
2020-10-17 16:50:13 +0800

    :sparkles: :bento: :recycle:  Changed python3 `asyncio advanced -> subprocess protocol` example

--- 29_examples_asyncio_advanced/asyncio_advanced_subprocess_protocol.py
+++ 29_examples_asyncio_advanced/asyncio_advanced_subprocess_protocol.py
@@ -3,7 +3,7 @@
 # @author: hoojo
 # @email: hoojo_@126.com
 # @github: https://github.com/hooj0
-# @create date: 2020-10-14
+# @create date: 2020-10-15
 # @copyright by hoojo@2018
 # @changelog python3 `asyncio advanced -> subprocess protocol` example
 
