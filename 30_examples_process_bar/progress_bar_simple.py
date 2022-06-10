a7445eb138763a652b11a62c6d7c5aaf9fbfca95
hoojo <hoojo@qq.com>
2022-06-10 12:04:23 +0800

    :sparkles: :bento: :recycle:  Changed python3 `progress bar` example

--- 30_examples_process_bar/progress_bar_simple.py
+++ 30_examples_process_bar/progress_bar_simple.py
@@ -97,3 +97,38 @@ for i in range(1, 61):
 # output:
 # ---------------------------------------------------------------------------
 # ############################################################
+
+
+
+# -------------------------------------------------------------------------------
+# 固定在底部的进度条
+# -------------------------------------------------------------------------------
+class ProgressBar:
+    def __init__(self, count=0, total=0, width=50):
+        self.count = count
+        self.total = total
+        self.width = width
+
+    def move(self):
+        self.count += 1
+
+    def log(self, s):
+        sys.stdout.write(' ' * (self.width + 9))
+        sys.stdout.write('\r')
+        sys.stdout.flush()
+        print(s)
+        progress = int(self.width * self.count / self.total)
+        sys.stdout.write('{0:3}/{1:3}: '.format(self.count, self.total))
+        sys.stdout.write('\r')
+        sys.stdout.write('#' * progress + '-' * (self.width - progress))
+        if progress == self.width:
+            sys.stdout.write('\n')
+        sys.stdout.flush()
+
+
+total = 10
+bar = ProgressBar(total=total)
+for i in range(total):
+    bar.move()
+    bar.log('We have arrived at: ' + str(i + 1))
+    time.sleep(1)
