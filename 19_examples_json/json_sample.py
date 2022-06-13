ae2b123b287d53a92ef3c61383e6ff0e6b0fce9b
hoojo <hoojo@qq.com>
2022-06-13 11:42:15 +0800

    :sparkles: :bento:  Added  python3 `json lib` example

--- 19_examples_json/json_sample.py
+++ 19_examples_json/json_sample.py
@@ -60,6 +60,12 @@ json_data = json.loads(json_str)
 print ("json_data['name']: ", json_data['name'])
 print ("json_data['url']: ", json_data['url'])
 
+# 格式化输出JSON
+# sort_keys：是否按照字典排序（a-z）输出，True代表是，False代表否。
+# indent=4：设置缩进格数，一般由于Linux的习惯，这里会设置为4。
+# separators：设置分隔符，在dic = {'a': 1, 'b': 2, 'c': 3}这行代码里可以看到冒号和逗号后面都带了个空格，这也是因为Python的默认格式也是如此，如果不想后面带有空格输出，那就可以设置成separators=(',', ':')，如果想保持原样，可以写成separators=(', ', ': ')
+# ensure_ascii=False ：是否显示ascii这个码，默认是ture，改为False 即可
+print(json.dumps(json_data, sort_keys=True, indent=4, separators=(',', ':'), ensure_ascii=False))
 
 
 #如果你要处理的是文件而不是字符串，你可以使用 json.dump() 和 json.load() 来编码和解码JSON数据。例如：
